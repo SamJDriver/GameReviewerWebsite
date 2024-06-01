11b3b8e5c3073d7c203f3c1614b3d9a213a29581
nick <nicksundvall@gmail.com>
2024-06-01 11:58:52 -0700

    Removed audit values and IDs from CreatePlayRecordCommentJson.cs

--- GameReviewBackend/API/Models/CreatePlayRecordCommentJson.cs
+++ GameReviewBackend/API/Models/CreatePlayRecordCommentJson.cs
@@ -4,15 +4,11 @@ namespace API.Models
 {
     public class CreatePlayRecordCommentJson
     {
-        public int? Id { get; set; }
-        public int UserId { get; set; }
         public int PlayRecordId { get; set; }
         [StringLength(65535)]
         public string CommentText { get; set; } = null!;
         public int UpvoteCount { get; set; } = 0;
         public int DownvoteCount { get; set; } = 0;
         [StringLength(25)]
-        public string CreatedBy { get; set; } = default!;
-        public DateTime CreatedDate { get; set; } = default;
     }
 }

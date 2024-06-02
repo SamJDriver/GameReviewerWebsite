9e4319d23012ed8a416abab4e5715c98d4fac00e
nick <nicksundvall@gmail.com>
2024-06-01 18:04:20 -0700

    bug fix

--- GameReviewBackend/API/Models/CreatePlayRecordCommentJson.cs
+++ GameReviewBackend/API/Models/CreatePlayRecordCommentJson.cs
@@ -9,6 +9,6 @@ namespace API.Models
         public string CommentText { get; set; } = null!;
         public int UpvoteCount { get; set; } = 0;
         public int DownvoteCount { get; set; } = 0;
-        [StringLength(25)]
+      
     }
 }

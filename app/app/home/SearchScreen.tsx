da7de53d6392602eaaa89683c129df8cd58bc5d2
nick <nicksundvall@gmail.com>
2024-06-10 22:07:35 -0700

    Added search bar and back button to SearchScreen

--- app/app/home/SearchScreen.tsx
+++ app/app/home/SearchScreen.tsx
@@ -1,9 +1,48 @@
+import React from 'react';
 import { View, Text } from 'react-native';
+import { IconButton, MD3Colors, Searchbar } from 'react-native-paper';
 
-export default function SearchScreen() {
+const SearchScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
   return (
     <View>
+      <View
+        style={{ flexDirection: 'row', justifyContent:'center' }}>
+          <View style={{ flex: 1}}>
+        <IconButton
+          icon="arrow-left"
+          iconColor={MD3Colors.error50}
+          size={20}
+          onPress={() => console.log('Pressed iconbutton')}
+        />
+        </View>
+        <View style={{ flex: 9}}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          mode="view"
+        />
+        </View>
+      </View>
       <Text>Search Screen</Text>
     </View>
   );
 }
+
+export default SearchScreen;
+
+// const MyComponent = () => {
+//   const [searchQuery, setSearchQuery] = React.useState('');
+
+//   return (
+//     <Searchbar
+//       placeholder="Search"
+//       onChangeText={setSearchQuery}
+//       value={searchQuery}
+//     />
+//   );
+// };
+
+// export default MyComponent;

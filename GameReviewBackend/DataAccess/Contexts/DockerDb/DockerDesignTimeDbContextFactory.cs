a99b566e295263f9438fbea3417d5f2d48e3929e
Sam <samjdriv@gmail.com>
2024-06-13 23:52:58 -0700

    fixed path in migration. added comments for guidelines on using migration

--- GameReviewBackend/DataAccess/Contexts/DockerDb/DockerDesignTimeDbContextFactory.cs
+++ GameReviewBackend/DataAccess/Contexts/DockerDb/DockerDesignTimeDbContextFactory.cs
@@ -6,15 +6,20 @@ namespace DataAccess.Contexts.DockerDb
 {
     public class DockerDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DockerDbContext>
     {
+        // This method is called by the runtime.
+        // Use the following commands to perform the following:
+        // release: dotnet ef migrations script --idempotent --context DockerDbContext
+        // update your local database with migrations: dotnet ef database update --context DockerDbContext
+        // update your local database with seed data: dotnet ef database update --context DockerDbContext --seed
+        // update your loca database with a generated script: dotnet ef migrations script --idempotent --context DockerDbContext
+
         public DockerDbContext CreateDbContext(string[] args)
         {
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "../../../API");
-            Console.WriteLine(path);
-
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "../API");
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
             IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../../API"))
+                .SetBasePath(path)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: true)
                 .AddUserSecrets("34a2eb48-f55e-4322-8205-5f51e2572770")

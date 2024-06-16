b3a39e30be10a61f3986e74b3badba06c3aff467
Sam <samjdriv@gmail.com>
2024-06-16 11:05:54 -0700

    Deleted unnecessary migration generated files.

--- GameReviewBackend/Scripts/db-migration-create.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    [Parameter(
-        Mandatory = $true,
-        HelpMessage = "Unique name for the migration to be created")]
-        [string]$MigrationName
-    )
-    
-. $PSScriptRoot\get-user-secret.ps1

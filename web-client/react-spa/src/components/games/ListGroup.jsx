6e282690662eea05550161bfc6dc8c073a7576b5
Sam <samjdriv@gmail.com>
2024-06-17 20:00:00 -0700

    Added react client back without git subnode

---
+++ web-client/react-spa/src/components/games/ListGroup.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from 'react';
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Los Angeles", "Chicago", "Dallas"];
+
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>DGC User</h1>
+      {items.length === 0 && <p>No items found.</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li key={item} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={() => {setSelectedIndex(index)}}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;

6bd6fdbb69181f029ed19c8df8a0bc06a69a277c
nick <nicksundvall@gmail.com>
2024-07-15 23:16:47 -0700

    Add new buttons to drawer and working navigation

--- app/app/home/_layout.tsx
+++ app/app/home/_layout.tsx
@@ -2,8 +2,8 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 import { MD3DarkTheme } from 'react-native-paper';
 import { useEffect } from "react";
-import { usePathname } from "expo-router";
-import { DrawerContentScrollView } from "@react-navigation/drawer";
+import { router, usePathname } from "expo-router";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import { View, Image, Text } from "react-native";
 
 const CustomDrawerContent = (props) => {
@@ -15,18 +15,51 @@ const CustomDrawerContent = (props) => {
 // https://github.com/itzpradip/basic-expo-router/blob/main/app/(drawer)/_layout.js
   return (
     <DrawerContentScrollView {...props}>
-      <View>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        {/* TODO put users profile pic */}
         <Image
           source={require('./../../assets/images/react-logo.png')}
           width={80}
           height={80}
         />
-        <View>
-          <Text>John Doe</Text>
-          <Text>john@email.com</Text>
+        <View style={{ flexDirection: "column", justifyContent: "center" }}>
+
+          {/* TODO put the actual username */}
+          <Text style={{ color: 'white' }}>Username</Text>
         </View>
+        
+
       </View>
-      
+      <DrawerItem
+        label="Home"
+        labelStyle={{color: 'white' }}      
+        onPress={() => router.navigate('home/(tabs)/')}
+      />
+      <DrawerItem
+        label="Profile"
+        labelStyle={{color: 'white' }}      
+        onPress={() => router.navigate('home/(tabs)/ProfileScreen')}
+      />
+      <DrawerItem
+        label="Reviews"
+        labelStyle={{color: 'white' }}      
+        onPress={() => router.navigate('home/(tabs)/ReviewsScreen/')}
+      />
+      <DrawerItem
+        label="Games"
+        labelStyle={{color: 'white' }}      
+        onPress={() => router.navigate('home/(tabs)/GamesScreen/')}
+      />
+      <DrawerItem
+        label="Settings"
+        labelStyle={{color: 'white' }}      
+        onPress={() => router.navigate('home/settings/')}
+      />
+      <DrawerItem
+        label="Sign Out"
+        labelStyle={{color: 'white' }}    
+        onPress={() => console.log('Pressed Sign Out button')}
+      />
     </DrawerContentScrollView>
   );
 };

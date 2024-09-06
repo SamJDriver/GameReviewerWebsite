3dd4638d7f78bac486d1868a4b57f8cab3f8169f
SamJDriver <SamJDriv@gmail.com>
2024-09-05 22:31:21 -0700

    Pretty much finished styling calendars for game search

--- web-client/react-spa/src/components/GameSearch/GameSearch.tsx
+++ web-client/react-spa/src/components/GameSearch/GameSearch.tsx
@@ -1,45 +1,49 @@
 import { BsSearch } from "react-icons/bs";
+import { BsCalendarEvent } from "react-icons/bs";
 import { IGenre } from "../../interfaces/IGenre";
 import { BASE_URL } from "../../UrlProvider";
 import { useEffect, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import './GameSearch.css';
+import { Calendar } from "react-date-range";
+
+interface IReleaseYearRange {
+  startYear: string;
+  endYear: string;
+}
 
 export const GameSearch = () => {
   const [genres, setGenres] = useState<IGenre[]>();
-  const [selectedGenres, setSelectedGenres] = useState<string[] | null>(null);
-  const [releaseYears, setReleaseYears] = useState<number[] | null>(null);
+  const [releaseYearRange, setReleaseYearRange] = useState<IReleaseYearRange | null>(null);
+  const [startReleaseYearDatePickerOpen, setStartReleaseYearDatePickerOpen] = useState<boolean>(false);
+  const [endReleaseYearDatePickerOpen, setEndReleaseYearDatePickerOpen] = useState<boolean>(false);
 
+  const [selectedGenres, setSelectedGenres] = useState<string[] | null>(null);
+  const [selectedMinReleaseDate, setSelectedMinReleaseDate] = useState<Date | null>(null);
+  const [selectedMaxReleaseDate, setSelectedMaxReleaseDate] = useState<Date | null>(null);
 
   useEffect(() => {
       fetch(BASE_URL + '/lookup/genres')
           .then(response => response.json())
           .then(data => setGenres(data));
-  }, []);
-
-    useEffect(() => {
-      fetch(BASE_URL + '/lookup/release-years')
+      fetch(BASE_URL + '/lookup/release-year-range')
           .then(response => response.json())
-          .then(data => setReleaseYears(data));
+          .then(data => setReleaseYearRange(data));
   }, []);
 
-
-  if (!genres || !releaseYears) { 
+  if (!genres || !releaseYearRange) {
     return <div> Loading...</div>
   }
 
   const GenreCheckboxOnClick = (genreName: string) => {
-    if (!genreName) {
-      console.log('reeererere');
-      return;
-    }
-
     if (!selectedGenres) {
       setSelectedGenres([genreName]);
       return;
     }
-    
+
     const copy = selectedGenres.slice();
     const index: number = copy?.indexOf(genreName) ?? -1;
     if (index !== -1) {
@@ -51,11 +55,31 @@ export const GameSearch = () => {
     setSelectedGenres(copy);
   }
 
-  
+  document.body.addEventListener('click', function(event: MouseEvent) {
+    if (!(event.target as Element).closest('.search-filter--date-picker')) {
+      const calendarElement = document.getElementById('startReleaseDateCalendar') as HTMLElement | null;
+      const clickedElementId = (event.target as Element).id;
+      if (calendarElement && clickedElementId !== 'openStartReleaseDateCalendar' && clickedElementId !== 'startReleaseDateCalendar' && clickedElementId !=='startCalendarIcon') {
+        setStartReleaseYearDatePickerOpen(false);
+      } 
+    }
+  });
+
+  document.body.addEventListener('click', function(event: MouseEvent) {
+    if (!(event.target as Element).closest('.search-filter--date-picker')) {
+      const calendarElement = document.getElementById('endReleaseDateCalendar') as HTMLElement | null;
+      const clickedElementId = (event.target as Element).id;
+      if (calendarElement && clickedElementId !== 'openEndReleaseDateCalendar' && clickedElementId !== 'endReleaseDateCalendar' && clickedElementId !=='endCalendarIcon') {
+        setEndReleaseYearDatePickerOpen(false);
+      } 
+    }
+  });
+
+
   return (
-    <>    
+    <>
         <div className="search-filter--container">
-          <div className="search-filter--text-box-container input-group text-secondary mx-4" >
+          <div className="input-group mx-4" >
             <div className="input-group-prepend input-group-text search-filter--item-background-color">
               <BsSearch/>
             </div>
@@ -69,14 +93,14 @@ export const GameSearch = () => {
                 <Form>
                   <Dropdown.Menu>
                     {
-                      genres.map( (g: IGenre, index: number) => { 
+                      genres.map( (g: IGenre, index: number) => {
                         if (!g) {
                           return <Dropdown.Item key={index}> Loading ... </Dropdown.Item>
                         }
                         return (
-                          <Form.Check type='checkbox' key={'genre-check'+index} id={'genre-check-id'+index} className=" search-filter--item-background-color" label={g.name} onClick={() => GenreCheckboxOnClick(g.name)}> 
+                          <Form.Check type='checkbox' key={'genre-check'+index} id={'genre-check-id'+index} className=" search-filter--item-background-color" label={g.name} onClick={() => GenreCheckboxOnClick(g.name)}>
                           </Form.Check>
-                        ) 
+                        )
                       })
                     }
                   </Dropdown.Menu>
@@ -88,31 +112,35 @@ export const GameSearch = () => {
             </div>
           </div>
 
-          <div className="search-filter--text-box-container input-group mx-4" >
-            <div className="input-group-text search-filter--item-background-color">
-              <Dropdown>
-                <Dropdown.Toggle variant="dark" className="search-filter--genre-toggle"  id="dropdown-basic"> Release Year </Dropdown.Toggle>
-                <Form>
-                  <Dropdown.Menu>
-                  {
-                    releaseYears.map( (releaseYear: number, index: number) => { 
-                      if (!releaseYear) {
-                        return <Dropdown.Item key={index}> Loading ... </Dropdown.Item>
-                      }
-                      return (<div key={index}>
-                        <Form.Check type='checkbox' className="search-filter--item-background-color" label={releaseYear} />                    
-                        </div>)                  
-                    })
-                  }
-                  </Dropdown.Menu>
-                </Form>
-              </Dropdown>
+          <div className="input-group mx-4" >
+            <div onClick={() => setStartReleaseYearDatePickerOpen(!startReleaseYearDatePickerOpen)} id="openStartReleaseDateCalendar" className="input-group-prepend input-group-text search-filter--item-background-color">
+              <BsCalendarEvent id="startCalendarIcon"/>
             </div>
-            <div id="genreTextInput" className="search-filter--item-background-color form-control">
+            <input placeholder="Start Release Date" className="form-control search-filter--item-background-color" aria-label="search start date" />
+            <div id="startReleaseDateCalendar" className="input-group-text" style={{backgroundColor: "rgb(26, 28, 30)", borderStyle: "none"}}>
+              <Calendar
+                minDate={new Date(`${releaseYearRange.startYear}-01-01`)} 
+                maxDate={new Date(`${releaseYearRange.endYear}-12-31`)} 
+                className={startReleaseYearDatePickerOpen ? "search-filter--date-picker" : "search-filter--date-picker-hide"} 
+                onChange={ (event) => console.log(event)} /> 
+            </div>
+          </div>
+
+          <div className="input-group mx-4">
+            <div onClick={() => setEndReleaseYearDatePickerOpen(!endReleaseYearDatePickerOpen)} id="openEndReleaseDateCalendar" className="input-group-prepend input-group-text search-filter--item-background-color">
+              <BsCalendarEvent id="endCalendarIcon"/>
+            </div>
+            <input placeholder="End Release Date" className="form-control search-filter--item-background-color" aria-label="search end date" />
+            <div id="endReleaseDateCalendar" className="input-group-text" style={{backgroundColor: "rgb(26, 28, 30)", borderStyle: "none"}}>
+              <Calendar
+                minDate={new Date(`${releaseYearRange.startYear}-01-01`)} 
+                maxDate={new Date(`${releaseYearRange.endYear}-12-31`)} 
+                className={endReleaseYearDatePickerOpen ? "search-filter--date-picker" : "search-filter--date-picker-hide"} 
+                onChange={ (event) => console.log(event)} /> 
             </div>
           </div>
         </div>
     </>
   )
-}                    
+}
 

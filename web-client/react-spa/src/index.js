aff54ed537c17e34e7a175f51b240848c047a362
SamJDriver <SamJDriv@gmail.com>
2024-09-16 18:40:28 -0700

    Removed strict mode tag on render

--- web-client/react-spa/src/index.js
+++ web-client/react-spa/src/index.js
@@ -43,11 +43,9 @@ msalInstance.initialize().then(() => {
  * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
  */
     root.render(
-        <React.StrictMode>
-            <MsalProvider instance={msalInstance}>
-                <App />
-            </MsalProvider>
-        </React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+          <App />
+      </MsalProvider>
     );
   });
   

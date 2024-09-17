00c9cb75c76a59f797a41d2bd542e9d71de3583b
SamJDriver <SamJDriv@gmail.com>
2024-09-16 18:44:48 -0700

    Added some wild code for displaying +1, +2, etc... on the checkbox component

--- web-client/react-spa/src/components/CheckboxDropdownList/CheckboxDropdownList.tsx
+++ web-client/react-spa/src/components/CheckboxDropdownList/CheckboxDropdownList.tsx
@@ -3,22 +3,23 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import { useState } from 'react';
 import IDropdownItem from '../../interfaces/IDropdownItem';
 import classnames from 'classnames';
-
-
+import { MdClear } from 'react-icons/md';
 
 interface IProps {
     className?: string;
     items: IDropdownItem[];
     heading: string;
-    onItemSelected: (item: IDropdownItem[]) => void;
+    onItemSelected: (item: IDropdownItem[] | null) => void;
 }
 
 export const CheckboxDropdownList = (props: IProps) => {
-    const [selectedItems, setSelectedItems] = useState<IDropdownItem[] | null>(null);
+    const [selectedItems, setSelectedItems] = useState<IDropdownItem[] | null>([]);
+    const [displayItems, setDisplayItems] = useState<string[] | null>(null);
 
     const ItemCheckboxOnClick = (item: IDropdownItem) => {
-      if (!selectedItems) {
+      if (!selectedItems || selectedItems?.length === 0) {
         setSelectedItems([item]);
+        setDisplayItems([item.name]);
         props.onItemSelected([item]);
         return;
       }
@@ -27,17 +28,28 @@ export const CheckboxDropdownList = (props: IProps) => {
       const index: number = copy?.indexOf(item) ?? -1;
       
       if (index !== -1) {
+
         copy?.splice(index, 1);
+        const itemsFromCopy = copy.map((item: IDropdownItem) => item.name).slice(0, 1);
+        if (copy.length > 1){
+          itemsFromCopy.push("+ " + (copy.length - 1));
+        }
+        setDisplayItems(itemsFromCopy);
       }
       else {
         copy.push(item);
-      
+        if (copy.length > 1)
+          {
+            const itemsFromMap = copy.map((item: IDropdownItem) => item.name).slice(0, 1);
+            itemsFromMap.push("+ " + (copy.length - 1));
+            setDisplayItems(itemsFromMap);
+          }
       }
-
+      
       setSelectedItems(copy);
-      props.onItemSelected(copy);
+      props.onItemSelected(copy.length === 0 ? null : copy);
     }
-    
+
     const classes = classnames('input-group', props.className);
 
     return (
@@ -54,7 +66,13 @@ export const CheckboxDropdownList = (props: IProps) => {
                               return <Dropdown.Item key={index}> Loading ... </Dropdown.Item>
                             }
                             return (
-                              <Form.Check type='checkbox' key={'genre-check'+index} id={'genre-check-id'+index} className=" game-search--item-background-color" label={item.name} onClick={() => ItemCheckboxOnClick(item)}>
+                              <Form.Check 
+                                checked={selectedItems?.indexOf(item) !== -1}
+                                type='checkbox' key={'genre-check'+index}
+                                id={'genre-check-id'+index}
+                                className=" game-search--item-background-color"
+                                label={item.name} 
+                                onChange={() => { ItemCheckboxOnClick(item); }}>
                               </Form.Check>
                             )
                           })
@@ -64,7 +82,10 @@ export const CheckboxDropdownList = (props: IProps) => {
                   </Dropdown>
                 </div>
                 <div id="genreTextInput" className="game-search--text-box game-search--item-background-color form-control">
-                  {selectedItems?.map( (item: IDropdownItem, i: number) => <span className="game-search--genre-item mx-1" key={i} >{item.name}</span> )}
+                  {displayItems?.map( (item: string, i: number) => <span className="game-search--genre-item mx-1" key={i} >{item}</span> )}
+                </div>
+                <div onClick={() => { setSelectedItems([]); setDisplayItems(null); props.onItemSelected(null); } } className="input-group-append input-group-text game-search--item-background-color">
+                    <MdClear/>
                 </div>
             </div>
         </>

bf3facd5e2e28a214598c3dff63f3157265729cd
SamJDriver <SamJDriv@gmail.com>
2024-11-08 00:30:44 -0800

    My List table looking pretty good

--- web-client/react-spa/src/pages/MyList/MyList.tsx
+++ web-client/react-spa/src/pages/MyList/MyList.tsx
@@ -9,21 +9,39 @@ import { ListGroup } from "react-bootstrap";
 const MyList = () => {
     const baseUrl = useBaseUrlResolver();
     const { token } = useToken();
-    const {data: playRecords, error: playRecordsError, isLoading: playRecordsIsLoading }
-    = useSWR([`${baseUrl}/play-record`, token], ([url, token]) => fetcher(url, token))
+    const {data: playRecords, error: playRecordsError, isLoading: playRecordsIsLoading } 
+    = useSWR([`${baseUrl}/play-record`, token], ([url, token]) => fetcher(url, token));
 
-    // if (playRecordsError) {
-    //     return <div>Failed to load + {playRecordsError}</div>
-    //   }
+    if (playRecordsError) {
+        return <div>Failed to load + {playRecordsError.message}</div>
+    }
     
-    //   if (playRecordsIsLoading || !playRecords) {
-    //     return <div>Loading...</div>
-    //   }
+    if (playRecordsIsLoading || !playRecords) {
+      return <div>Loading...</div>
+    }
 
     return (
         <>
+        <div className="grid-container">
+            <div className="grid-header" style={{textAlign: 'left'}}>Title</div>
+            <div className="grid-header">Score</div>
+            <div className="grid-header">Hours</div>
+            <div className="grid-header">Completed?</div>
 
-            <div>My List</div>
+            {playRecords.map((playRecord: any, index: number) =>  (
+                <div key={index} className="grid-row">
+                    <div style={{textAlign: 'left', fontSize: '1em'}}>
+                        <img style={{width: '4em', height: 'auto'}} src={playRecord.coverImageUrl} />
+                        <span>{playRecord.gameTitle}</span>
+                    </div>
+                    <div className="grid-item">
+                        <span>{playRecord.rating}</span>
+                    </div>
+                    <div className="grid-item">{playRecord.hoursPlayed}</div>
+                    <div className="grid-item">{playRecord.completedFlag ? 'Yes' : 'No'}</div>
+                </div>
+            ))}
+        </div>
         </>
     );
 }

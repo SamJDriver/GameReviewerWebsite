29409bda805bd896c6221f7ad823108184537abd
SamJDriver <SamJDriv@gmail.com>
2025-03-11 00:20:12 -0700

    Fixed bug with user timeout crashing the site

--- web-client/react-spa/src/utils/useToken.tsx
+++ web-client/react-spa/src/utils/useToken.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {  useIsAuthenticated, useMsal } from '@azure/msal-react';
 import { loginRequest } from '../authConfig';
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
 
 export function useToken() {
   const { instance, accounts } = useMsal();
@@ -14,10 +15,24 @@ export function useToken() {
           account: currentAccount,
         };
         
-        instance.acquireTokenSilent(accessTokenRequest).then((response) => {
-          setToken(response.accessToken);
-      });
-      
+        
+          // instance.acquireTokenSilent(accessTokenRequest).then((response) => {
+          //   setToken(response.accessToken);
+          // });
+
+          instance.acquireTokenSilent(accessTokenRequest)
+          .then(function (accessTokenResponse) {
+            // Acquire token silent success
+            let accessToken = accessTokenResponse.accessToken;
+            setToken(accessToken);
+          })
+          .catch(function (error) {
+            //Acquire token silent failure, and send an interactive request
+            console.log(error);
+            if (error instanceof InteractionRequiredAuthError) {
+              instance.acquireTokenRedirect(accessTokenRequest);
+            }
+          });
   }, []);
 
   return { token: token };
